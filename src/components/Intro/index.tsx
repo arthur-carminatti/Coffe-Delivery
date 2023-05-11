a0543820d3b8784cd4f4ecb8733382e8fdf98610
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-05-11 18:18:35 -0300

    responsive

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,4 +1,5 @@
 import {
+    IntroAlignContainer,
     IntroContainer,
     IntroTitleContainer,
     ItenFourIntroContainer,
@@ -19,36 +20,38 @@ import imgIntro from '../../assets/imgIntro.svg'
 
 export function Intro() {
     return (
-        <IntroContainer >
-            <IntroTitleContainer>
-                <span>Encontre o café perfeito para qualquer hora do dia</span>
-                <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
-            </IntroTitleContainer>
+        <IntroAlignContainer>
+            <IntroContainer >
+                <IntroTitleContainer>
+                    <span>Encontre o café perfeito para qualquer hora do dia</span>
+                    <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+                </IntroTitleContainer>
 
-            <img src={imgIntro} alt="" />
+                <img src={imgIntro} alt="" />
 
-            <ItensInfoIntroContainer>
+                <ItensInfoIntroContainer>
 
-                <ItenOneIntroContainer>
-                    <ShoppingCart size={16} weight="fill" />
-                    <p>Compra simples e segura </p>
-                </ItenOneIntroContainer>
+                    <ItenOneIntroContainer>
+                        <ShoppingCart size={16} weight="fill" />
+                        <p>Compra simples e segura </p>
+                    </ItenOneIntroContainer>
 
-                <ItenTwoIntroContainer>
-                    <Package size={16} weight="fill" />
-                    <p>Embalagem mantém o café intacto </p>
-                </ItenTwoIntroContainer>
+                    <ItenTwoIntroContainer>
+                        <Package size={16} weight="fill" />
+                        <p>Embalagem mantém o café intacto </p>
+                    </ItenTwoIntroContainer>
 
-                <ItenThreeIntroContainer>
-                    <Timer size={16} weight="fill" />
-                    <p>Entrega rápida e rastreada </p>
-                </ItenThreeIntroContainer>
+                    <ItenThreeIntroContainer>
+                        <Timer size={16} weight="fill" />
+                        <p>Entrega rápida e rastreada </p>
+                    </ItenThreeIntroContainer>
 
-                <ItenFourIntroContainer>
-                    <Coffee size={16} weight="fill" />
-                    <p>O café chega fresquinho até você </p>
-                </ItenFourIntroContainer>
-            </ItensInfoIntroContainer>
-        </IntroContainer>
+                    <ItenFourIntroContainer>
+                        <Coffee size={16} weight="fill" />
+                        <p>O café chega fresquinho até você </p>
+                    </ItenFourIntroContainer>
+                </ItensInfoIntroContainer>
+            </IntroContainer>
+        </IntroAlignContainer>
     )
 }
--- src/components/Intro/styles.ts
+++ src/components/Intro/styles.ts
@@ -1,15 +1,21 @@
 import styled from "styled-components";
 
+export const IntroAlignContainer = styled.div`
+    width: 100%;
+    height: 34rem;
+    position: fixed;
+`
+
 export const IntroContainer = styled.section`
     width: 100%;
-    height: 544px;
+    height: 34rem;
 
     img {
         position: absolute;
-        width: 476px;
-        height: 360px;
-        left: calc(50% - 476px/2 + 322px);
-        top: calc(50% - 360px/2);
+        width: 29.75rem;
+        height: 22.5rem;
+        left: calc(50% - 29.75rem/2 + 20.125rem);
+        top: calc(50% - 22.5rem/2);
     }
 `
 
@@ -18,21 +24,21 @@ export const IntroTitleContainer = styled.div`
     flex-direction: column;
     align-items: flex-start;
     padding: 0px;
-    gap: 16px;
+    gap: 1rem;
 
     position: absolute;
-    width: 588px;
-    height: 192px;
-    left: calc(50% - 588px/2 - 266px);
-    top: calc(50% - 192px/2 - 82px);
+    width: 36.75rem;
+    height: 12rem;
+    left: calc(50% - 36.75rem/2 - 16.625rem);
+    top: calc(50% - 12rem/2 - 5.125rem);
 
     span {
         font-weight: 800;
-        font-size: 48px;
+        font-size: 3rem;
         line-height: 130%;
 
-        width: 588px;
-        height: 124px;
+        width: 36.75rem;
+        height: 7.75rem;
         align-self: stretch;
 
         color: ${props => props.theme["black-800"]};
@@ -40,11 +46,11 @@ export const IntroTitleContainer = styled.div`
     
     p {
         font-weight: 400;
-        font-size: 20px;
+        font-size: 1.25rem;
         line-height: 130%;
 
-        width: 588px;
-        height: 52px;
+        width: 36.75rem;
+        height: 3.25rem;
 
         font-stretch: 100;
 
@@ -54,10 +60,10 @@ export const IntroTitleContainer = styled.div`
 
 export const ItensInfoIntroContainer = styled.div`
     position: absolute;
-    width: 567px;
-    height: 84px;
-    left: calc(50% - 567px/2 - 276.5px);
-    top: calc(50% - 84px/2 + 122px);
+    width: 35.438rem;
+    height: 5.25rem;
+    left: calc(50% - 35.438rem/2 - 17.281rem);
+    top: calc(50% - 5.25rem/2 + 7.625rem);
 `
 
 export const ItenOneIntroContainer = styled.div`
@@ -65,32 +71,32 @@ export const ItenOneIntroContainer = styled.div`
     align-items: center;
     justify-content: center;
     padding: 0px;
-    gap: 12px;
+    gap: 0.75rem;
 
     position: absolute;
-    width: 231px;
-    height: 32px;
+    width: 14.438rem;
+    height: 2rem;
     left: 0px;
     top: 0px;
 
     p {
         color: ${props => props.theme["gray-500"]};
-        font-size: 16px;
+        font-size: 1rem;
     }
 
     svg {
         background: ${props => props.theme["yellow-700"]};
         color: white;
         border-radius: 99999px;
-        padding: 8px;
-        width: 32px;
-        height: 32px;
+        padding: 0.5rem;
+        width: 2rem;
+        height: 2rem;
     }
 `
 
 export const ItenTwoIntroContainer = styled(ItenOneIntroContainer)`
-    width: 294px;
-    left: 271px;
+    width: 18.375rem;
+    left: 16.938rem;
 
     svg {
         background: ${props => props.theme["gray-500"]};
@@ -98,10 +104,10 @@ export const ItenTwoIntroContainer = styled(ItenOneIntroContainer)`
 `
 
 export const ItenThreeIntroContainer = styled(ItenOneIntroContainer)`
-    width: 231px;
-    height: 32px;
+    width: 14.438rem;
+    height: 2rem;
     left: 0px;
-    top: 52px;
+    top: 3.25rem;
 
     svg {
         background: ${props => props.theme["yellow-500"]};
@@ -109,9 +115,9 @@ export const ItenThreeIntroContainer = styled(ItenOneIntroContainer)`
 `
 
 export const ItenFourIntroContainer = styled(ItenOneIntroContainer)`
-    width: 294px;
-    left: 265px;
-    top: 52px;
+    width: 18.375rem;
+    left: 16.563rem;
+    top: 3.25rem;
 
     svg {
         background: ${props => props.theme["purble-600"]};
