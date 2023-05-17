fb4c31711f092ee60ae1ec5bde632521cb49bb90
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-05-17 17:01:04 -0300

    Default Layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,10 @@
 import { CoffeList } from "../../components/CoffeList";
-import { Header } from "../../components/Header";
 import { Intro } from "../../components/Intro";
 import { HomeContainer } from "./styles";
 
 export function Home() {
     return (
         <HomeContainer>
-            <Header />
             <Intro />
             <CoffeList />
         </HomeContainer>
